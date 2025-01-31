29bb88d10c61d0d097aa78e4c52693d54f033285
caiogesserc <caiogesserc3@gmail.com>
2025-01-31 09:23:33 -0300

    refactor: :art: refactor layout components

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   outDir: "build",
+
   server: {
     port: 8080,
     host: true,
   },
+
+  integrations: [react({ experimentalReactChildren: true, include: ['**/react/*'] })],
 });
--- src/mock/user.js
+++ src/mock/user.js
@@ -1,17 +1,23 @@
-export const mockUserDev = {
-  id: 1,
-  name: "Developer Teste",
-  email: "developer@teste.com",
-  role: "developer",
-  isLoggedIn: true,
-  username: "developer-teste",
-};
+export const mockUserDev = [
+  {
+    id: 1,
+    name: "Developer Teste",
+    email: "developer@teste.com",
+    password: "123456",
+    role: "developer",
+    isLoggedIn: false,
+    username: "developer-teste",
+  },
+];
 
-export const mockUserClient = {
-  id: 1,
-  name: "Client Teste",
-  email: "client@teste.com",
-  role: "client",
-  isLoggedIn: false,
-  username: "client-teste",
-};
+export const mockUserClient = [
+  {
+    id: 1,
+    name: "Client Teste",
+    email: "client@teste.com",
+    role: "client",
+    password: "123456",
+    isLoggedIn: false,
+    username: "client-teste",
+  },
+];
